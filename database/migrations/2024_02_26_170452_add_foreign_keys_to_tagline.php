c091aec6dd5b6080bb9e12c62030a01a416bbe66
EFENDI <fendikrama56@gmail.com>
2024-02-27 00:35:37 +0700

    feat:make foreign keys

--- database/migrations/2024_02_26_170452_add_foreign_keys_to_tagline.php
+++ database/migrations/2024_02_26_170452_add_foreign_keys_to_tagline.php
@@ -11,12 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('tagline', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('service_id')->nullable()->index('fk_tagline_to_service');
-            $table->string('tagline');
-            $table->softDeletes();
-            $table->timestamps();
+        Schema::table('tagline', function (Blueprint $table) {
+            $table->foreign('service_id', 'fk_tagline_to_service')->references('id')
+            ->on('service')->onUpdate('CASCADE')->onDelete('CASCADE');
+
         });
     }
 
@@ -25,6 +23,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('tagline');
+        Schema::table('tagline', function (Blueprint $table) {
+            $table->dropForeign('fk_tagline_to_service');
+        });
     }
 };
