244fa5ff43a019da6c68c0456a9d593da468fad1
EFENDI <fendikrama56@gmail.com>
2025-08-10 23:25:54 +0700

    add: request validation my order

--- app/Http/Requests/Dashboard/MyOrder/UpdateMyOrderRequest.php
+++ app/Http/Requests/Dashboard/MyOrder/UpdateMyOrderRequest.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Requests\Dashboard\MyOrder;
 
+use App\Models\Order;
 use Illuminate\Foundation\Http\FormRequest;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Validation\Rule;
 
 class UpdateMyOrderRequest extends FormRequest
 {
@@ -11,7 +14,7 @@ class UpdateMyOrderRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +25,27 @@ class UpdateMyOrderRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'buyer_id' => [
+                'nullable', 'integer',
+            ],
+            'freelancer_id' => [
+                'nullable', 'integer',
+            ],
+            'service_id' => [
+                'nullable', 'integer',
+            ],
+            'file' => [
+                'required', 'mimes:zip', 'max:1024',
+            ],
+            'note' => [
+                'required', 'string', 'max:10000',
+            ],
+            'expired' => [
+                'nullable', 'date',
+            ],
+            'order_status_id' => [
+                'nullable', 'integer',
+            ],
         ];
     }
 }
