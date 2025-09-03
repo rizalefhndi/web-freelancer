697aa73fe0cca2f0b8a3c42aa729ce9c69645744
EFENDI <fendikrama56@gmail.com>
2025-09-03 22:24:26 +0700

    add: detail order

--- resources/views/pages/dashboard/order/detail.blade.php
+++ resources/views/pages/dashboard/order/detail.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('title', 'Detail Order')
+@section('title', ' Detail Order')
 
 @section('content')
 
@@ -12,7 +12,7 @@
                         My Services
                     </h2>
                     <p class="text-sm text-gray-400">
-                        3 Total Services
+                        {{ auth()->user()->service()->count() }} Total Services
                     </p>
                 </div>
                 <div class="col-span-4 lg:text-right">
@@ -51,7 +51,7 @@
 
                                     <!-- details heading -->
                                     <div class="details-heading">
-                                        <h1 class="text-2xl font-semibold">I Will Design WordPress eCommerce Modules</h1>
+                                        <h1 class="text-2xl font-semibold">{{ $order->service->title ?? '' }}</h1>
                                         <div class="my-3">
                                             @include('components.dashboard.rating')
                                         </div>
@@ -60,12 +60,13 @@
                                         <img :src="featured" alt="" class="rounded-lg cursor-pointer w-100" data-lity>
                                         <div class="flex overflow-x-scroll hide-scroll-bar dragscroll">
                                             <div class="flex mt-2 flex-nowrap">
-                                                <img :class="{'border-4 border-serv-button': active === 1}" @click="changeThumbnail('https://source.unsplash.com/_SgRNwAVNKw/1600x900/',1)" src="{{ url('https://source.unsplash.com/_SgRNwAVNKw/250x160/') }}" alt="" class="inline-block w-24 mr-2 rounded-lg cursor-pointer">
-                                                <img :class="{'border-4 border-serv-button': active === 2}" @click="changeThumbnail('https://source.unsplash.com/GXNo-OJynTQ/1600x900/',2)" src="{{ url('https://source.unsplash.com/GXNo-OJynTQ/250x160/') }}" alt="" class="inline-block w-24 mr-2 rounded-lg cursor-pointer">
-                                                <img :class="{'border-4 border-serv-button': active === 3}" @click="changeThumbnail('https://source.unsplash.com/x-HpilsdKEk/1600x900/',3)" src="{{ url('https://source.unsplash.com/x-HpilsdKEk/250x160/') }}" alt="" class="inline-block w-24 mr-2 rounded-lg cursor-pointer">
-                                                <img :class="{'border-4 border-serv-button': active === 4}" @click="changeThumbnail('https://source.unsplash.com/hLit2zL-Dhk/1600x900/',4)" src="{{ url('https://source.unsplash.com/hLit2zL-Dhk/250x160/') }}" alt="" class="inline-block w-24 mr-2 rounded-lg cursor-pointer">
-                                                <img :class="{'border-4 border-serv-button': active === 5}" @click="changeThumbnail('https://source.unsplash.com/i1VQZsU86ok/1600x900/',5)" src="{{ url('https://source.unsplash.com/i1VQZsU86ok/250x160/') }}" alt="" class="inline-block w-24 mr-2 rounded-lg cursor-pointer">
-                                                <img :class="{'border-4 border-serv-button': active === 6}" @click="changeThumbnail('https://source.unsplash.com/iEiUITs149M/1600x900/',6)" src="{{ url('https://source.unsplash.com/iEiUITs149M/250x160/') }}" alt="" class="inline-block w-24 mr-2 rounded-lg cursor-pointer">
+
+                                                @forelse ($thumbnail as $item)
+                                                    <img :class="{ 'border-4 border-serv-button': active === {{ $item->id }} }" @click="changeThumbnail('{{ url(Storage::url($item->thumbnail)) }}', {{ $item->id }})" src="{{ url(Storage::url($item->thumbnail)) }}" alt="thumbnail service" class="inline-block w-24 mr-2 rounded-lg cursor-pointer">
+                                                @empty
+                                                    {{-- empty --}}
+                                                @endforelse
+
                                             </div>
                                         </div>
                                     </div>
@@ -76,18 +77,19 @@
                                                 <h2 class="text-xl font-semibold">About This <span class="text-serv-button">Services</span></h2>
                                                 <div class="mt-4 mb-8 content-description">
                                                     <p>
-                                                        I will design wordpress ecommerce modules, professional website for you using WordPress! With this Services
+                                                        {{ $order->service->description ?? '' }}
                                                     </p>
                                                 </div>
                                                 <h3 class="my-4 text-lg font-semibold">Why choose my Service?</h3>
                                                 <ul class="mb-4 list-check">
-                                                    <li class="pl-10 my-2">Fast delivery</li>
-                                                    <li class="pl-10 my-2">Wide plugin support within WordPress</li>
-                                                    <li class="pl-10 my-2">I can design logos and such for your website</li>
-                                                    <li class="pl-10 my-2">Easily Communicate with me</li>
+                                                    @forelse ($advantage_service as $advantage_service_item)
+                                                        <li class="pl-10 my-2">{{ $advantage_service_item->advantage ?? '' }}</li>
+                                                    @empty
+                                                        {{-- empty --}}
+                                                    @endforelse
                                                 </ul>
                                                 <p class="mb-4">
-                                                    If you only require modifications made to an existing WordPress website that you have, I have a different Services for that, which you can find on my profile!
+                                                    {{ $service->note ?? '' }}
                                                 </p>
                                                 <p class="mb-4 font-medium">
                                                     Contact me to get started!
@@ -104,7 +106,7 @@
                                                     <circle cx="12" cy="12" r="8" stroke="#082431" stroke-width="1.5" />
                                                     <path d="M12 7V12L15 13.5" stroke="#082431" stroke-width="1.5" stroke-linecap="round" />
                                                 </svg>
-                                                7 Days Delivery
+                                                {{ $order->service->delivery_time ?? '' }} Days Delivery
                                             </div>
                                             <div class="flex-1 text-sm font-medium text-center">
                                                 <svg class="inline" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -114,7 +116,7 @@
                                                     <path d="M7 21.5L4.14142 18.6414C4.06332 18.5633 4.06332 18.4247 4.14142 18.3586L7 15.5" stroke="#082431" stroke-width="1.5" stroke-linecap="round" />
                                                     <path d="M16 3L18.8586 5.85858C18.9247 5.92468 18.9247 6.06332 18.8586 6.14142L16 9" stroke="#082431" stroke-width="1.5" stroke-linecap="round" />
                                                 </svg>
-                                                1 Revision Limit
+                                                {{ $order->service->revision_limit ?? '' }} Revision Limit
                                             </div>
                                         </div>
                                         <div class="px-4 pt-4 pb-2 features-list">
@@ -133,7 +135,7 @@
                                                         Price starts from:
                                                     </td>
                                                     <td class="mb-4 text-xl font-semibold text-right text-serv-button">
-                                                        Rp120.000
+                                                        {{ 'Rp '.number_format($order->service->price) ?? '' }}
                                                     </td>
                                                 </tr>
 
@@ -145,6 +147,7 @@
                                     <button type="submit" class="inline-flex justify-center px-3 py-2 mb-2 text-xs font-medium text-gray-700 bg-gray-100 border border-transparent rounded-lg hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
                                         Programming & Tech
                                     </button>
+
                                     <button type="submit" class="inline-flex justify-center px-3 py-2 mb-2 text-xs font-medium text-gray-700 bg-gray-100 border border-transparent rounded-lg hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
                                         Website Developer
                                     </button>
@@ -153,7 +156,8 @@
                                     <a href="#" class="inline-flex justify-center px-4 py-2 mr-4 text-sm font-medium text-gray-700 bg-white border border-gray-600 rounded-lg shadow-sm hover:bg-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300">
                                         See Reviews
                                     </a>
-                                    <a href="{{ route('member.service.edit', 1) }}" class="inline-flex justify-center px-4 py-2 text-sm font-medium text-white border border-transparent rounded-lg shadow-sm bg-serv-email hover:bg-serv-email-text focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-serv-email">
+
+                                    <a href="{{ route('member.service.edit', $order->service_id) }}" class="inline-flex justify-center px-4 py-2 text-sm font-medium text-white border border-transparent rounded-lg shadow-sm bg-serv-email hover:bg-serv-email-text focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-serv-email">
                                         Edit Service
                                     </a>
                                 </div>
@@ -168,7 +172,6 @@
 @endsection
 
 @push('after-script')
-
     <script>
         function gallery() {
             return {
@@ -181,5 +184,4 @@
             }
         }
     </script>
-
 @endpush
